8919dd402faaa34f8a5bf5fe9cc609d246aa8ec7
田世洋 <tianshiyang@icourt.cc>
2026-01-17 15:59:34 +0800

    feat: 优化异常

--- src/pkg/response/http_code.py
+++ src/pkg/response/http_code.py
@@ -12,7 +12,5 @@ class HttpCode(str, Enum):
     """HTTP基础业务状态码"""
     SUCCESS = "success"
     ERROR = "error"
-    LOGIN_EXPIRED = "login_expired" # 登录过期
     UNAUTHORIZED = "unauthorized"  # 未授权
-    FORBIDDEN = "forbidden"  # 无权限
     VALIDATE_ERROR = "validate_error"  # 数据验证错误
