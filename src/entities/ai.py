e8c709c5a21740f58c194f2edd2d605ebcb206e8
田世洋 <tianshiyang@icourt.cc>
2026-02-08 18:19:20 +0800

    feat: 优化AI技能

--- src/entities/ai.py
+++ src/entities/ai.py
@@ -13,7 +13,8 @@ from enum import Enum
 class Skills(str, Enum):
     """AI技能"""
     DATASET_RETRIEVER = ("dataset_retriever", "知识库检索")
-    TEXT_TO_SQL = ("text_to_sql", "文本转SQL")
+    WEB_SEARCH = ("web_search", "联网搜索")
+    # TEXT_TO_SQL = ("text_to_sql", "文本转SQL")
 
     def __new__(cls, value: str, label: str):
         obj = str.__new__(cls, value)
